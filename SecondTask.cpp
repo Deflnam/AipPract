4841def271a1248d038872a329b0b4cb546d6c57
Deflnam <burukov01@bk.ru>
2025-11-01 19:10:31 +0300

    With keyboard input

---
+++ SecondTask.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstddef>
+
+void free_convert(int ** result, size_t created)
+{
+  if (result == nullptr)
+  {
+    return;
+  }
+  else {
+    for (size_t i = 0; i < created; i++)
+    {
+        delete[] result[i];
+    }
+  }
+  delete[] result;
+}
+
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows)
+{
+  if (t == nullptr || lns == nullptr)
+  {
+    return nullptr;
+  }
+  size_t elements = 0;
+  for (size_t i = 0; i < rows; i++)
+  {
+    elements += lns[i];
+  }
+  if (elements != n)
+  {
+    return nullptr;
+  }
+  int ** result = new int * [rows];
+  size_t number_index = 0;
+  size_t created = 0;
+  try
+  {
+  for (size_t i = 0; i < rows; i++)
+  {
+    result[i] = new int [lns[i]];
+    for (size_t j = 0; j < lns[i]; j++)
+    {
+      result[i][j] = t[number_index];
+      number_index++;
+    }
+    created++;
+  }
+}
+catch (const std::bad_alloc &e)
+{
+  free_convert(result, created);
+  throw;
+}
+return result;
+}
+
+void output(int ** result, size_t rows, const size_t * lns)
+{
+    if (result != nullptr)
+    {
+      for (size_t i = 0; i < rows; i++)
+     {
+        std::cout << result[i][0];
+        for (size_t j = 1; j < lns[i]; j++)
+        {
+            std::cout << " " << result[i][j];
+        }
+        std::cout << "\n";
+     } 
+    }
+    else 
+    {
+        std::cerr << "Error\n";
+    }
+}
+
+int main() {
+    int numbers[12];
+    
+    
+    for (int i = 0; i < 12; i++) {
+        std::cin >> numbers[i];
+    }
+    
+    size_t n = 12;
+    size_t lns[] = {4, 2, 5, 1};
+    size_t rows = 4;
+    
+    int ** result = nullptr;
+    try
+    {
+      result = convert(numbers, n, lns, rows);
+    }
+    catch (const std::bad_alloc &e)
+    {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+    
+    std::cout << "Matrix created!\n";
+    output(result, rows, lns);
+    free_convert(result, rows);
+    
+    return 0;
+}
